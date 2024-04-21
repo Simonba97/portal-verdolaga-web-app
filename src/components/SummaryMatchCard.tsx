a94731a50e0d9cccdac0464395fa88259a4f433d
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-04-20 23:49:15 -0500

    Adjust display to Summary Card

--- src/components/SummaryMatchCard.tsx
+++ src/components/SummaryMatchCard.tsx
@@ -8,7 +8,7 @@ import { TypesStatusFixturesShort } from "../utils/TypesStatusFixtures";
 
 const SummaryMatchCard = ({ matchData }: { matchData: IFixtureResponse }) => {
     return (
-        <div className='w-[85%] py-2 ml-9 bg-[url("../src/assets/bgCardApp.jpg")] bg-center bg-cover flex flex-row text-center items-center m-auto' >
+        <div className='w-[85%] bg-gray-100 flex flex-row text-center items-center m-auto' >
             <div className='w-[60px] text-center bg-green-400 -ml-8'>
                 <div>
                     <p className='text-xl font-extralight'>
@@ -21,41 +21,42 @@ const SummaryMatchCard = ({ matchData }: { matchData: IFixtureResponse }) => {
                     </p>
                 </div>
             </div>
-
-            <Link className='w-full h-full m-auto cursor-pointer' to={`/detail-match?matchId=${matchData.fixture.id}`}>
+            <Link className='w-full py-2 m-auto cursor-pointer' to={`/detail-match?matchId=${matchData.fixture.id}`}>
                 {/* BODY */}
-                <div>
-                    <div className="space-y-[-6px] mb-1">
-                        <p className='text-sm font-light not-italic'>
-                            <span>{`${matchData.league.name} - ${matchData.league.season}`}</span>
-                        </p>
-                        <p className='text-xs font-light not-italic'>
-                            <span>{matchData.league.round}</span>
-                        </p>
+                <div className="space-y-[-6px] mb-1">
+                    <p className='text-sm font-light not-italic'>
+                        <span>{`${matchData.league.name} - ${matchData.league.season}`}</span>
+                    </p>
+                    <p className='text-xs font-light not-italic'>
+                        <span>{matchData.league.round}</span>
+                    </p>
+                </div>
+                {/* Team Home Information */}
+                <div className="flex items-center">
+                    {/* Result/Time/Info */}
+                    <div className="flex w-2/5 justify-end items-center mr-2 space-x-1">
+                        <span className={`text-sm text-right leading-[16px] ${matchData.teams.home.id === Global.NACIONAL_ID_API_FOOTBALL ? 'font-normal' : 'font-extralight'}`}>{matchData.teams.home.name}</span>
+                        <img src={matchData.teams.home.logo} alt={matchData.teams.home.name} className='h-8' />
                     </div>
-                    <div className="flex items-center">
-                        <div className="flex items-center mr-2 space-x-1">
-                            <span className={`w-16 text-sm text-right leading-[16px] ${matchData.teams.home.id === Global.NACIONAL_ID_API_FOOTBALL ? 'font-normal' : 'font-extralight'}`}>{matchData.teams.home.name}</span>
-                            <img src={matchData.teams.home.logo} alt={matchData.teams.home.name} className='h-8' />
-                        </div>
+                    <div className="w-1/5 items-center">
 
                         {/* Match Finished */}
                         {matchData.fixture.status.short === TypesStatusFixturesShort.MatchFinished &&
-                            <span className='text-base font-light not-italic'>
+                            <span className='text-base font-light not-italic h-full'>
                                 {`${matchData.goals.home} - ${matchData.goals.away}`}
                             </span>
                         }
 
                         {/* Match Not Started */}
                         {matchData.fixture.status.short === TypesStatusFixturesShort.NotStarted &&
-                            <span className='text-base font-light not-italic'>
+                            <span className='text-base font-light not-italic h-full'>
                                 {format(new Date(matchData.fixture.date), 'HH:HH')}
                             </span>
                         }
 
                         {/* Match PostPoned */}
                         {matchData.fixture.status.short === TypesStatusFixturesShort.MatchPostponed &&
-                            <span className='text-xs font-light not-italic leading-[16px] bg-yellow-200'>
+                            <span className='text-xs font-light not-italic leading-[16px] bg-yellow-200 h-full'>
                                 {`Aplazado`}
                             </span>
                         }
@@ -63,14 +64,15 @@ const SummaryMatchCard = ({ matchData }: { matchData: IFixtureResponse }) => {
 
                         {/* Match Time to be Defined */}
                         {matchData.fixture.status.short === TypesStatusFixturesShort.TimeToBeDefined &&
-                            <span className='text-xs font-light not-italic leading-[16px] bg-red-200'>
+                            <span className='text-xs font-light not-italic leading-[16px] bg-red-200 h-full'>
                                 {`Sin horario`}
                             </span>
                         }
-                        <div className="flex items-center ml-2 space-x-1">
-                            <img src={matchData.teams.away.logo} alt={matchData.teams.away.name} className='h-8' />
-                            <span className={`w-16 text-sm text-left leading-[16px] ${matchData.teams.away.id === Global.NACIONAL_ID_API_FOOTBALL ? 'font-normal' : 'font-extralight'} `}>{matchData.teams.away.name}</span>
-                        </div>
+                    </div>
+                    {/* Team Away Information */}
+                    <div className="flex w-2/5 justify-start items-center ml-2 space-x-1">
+                        <img src={matchData.teams.away.logo} alt={matchData.teams.away.name} className='h-8' />
+                        <span className={`text-sm text-left leading-[16px] ${matchData.teams.away.id === Global.NACIONAL_ID_API_FOOTBALL ? 'font-normal' : 'font-extralight'} `}>{matchData.teams.away.name}</span>
                     </div>
                 </div>
             </Link >
