8503eab9b44acf64ae51932cf700e04ce1d0df77
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-08-12 23:21:38 -0500

    Implementation of leaderboard functionality on the leaderboard page

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -89,7 +89,7 @@ const SideBar = () => {
 
                     {/* Standings option */}
                     <li id="standingOption" >
-                        <Link to={'/' + appendParams} className="w-full flex space-x-4">
+                        <Link to={'/standings' + appendParams} className="w-full flex space-x-4">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" className="w-10 sm:w-7 stroke-gray-600"><path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" /></svg>
                             <span id="calendarLabel">Tabla de posiciones</span>
                         </Link>
