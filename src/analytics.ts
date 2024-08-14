c701592c06ec06af38a61559d50af58d5d3856ab
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-08-14 17:25:10 -0500

    Remove google analytics

--- src/analytics.ts
+++
@@ -1,9 +0,0 @@
-import ReactGA from "react-ga4";
-
-export const initGA = (measurementId: string) => {
-    ReactGA.initialize(measurementId);
-};
-
-export const logPageView = () => {
-    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-};
--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,6 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { NextMatchProvider } from './contexts/NextMatchContext.js';
 
-/* Google analytics imports */
-import { initGA, logPageView } from './analytics';
-
-const MEASUREMENT_ID = 'G-ESMZDF2H5M'; // Reemplaza con tu ID real
-
-initGA(MEASUREMENT_ID);
-logPageView();
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
