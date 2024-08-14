ec20ff77e575dc419e576787ebb65d382e2faafa
Simón Bustamante Alzate <simonba97@hotmail.com>
2024-08-14 17:29:33 -0500

    Add vercel analytics feature

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.js'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { NextMatchProvider } from './contexts/NextMatchContext.js';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <NextMatchProvider>
+        <Analytics />
         <App />
       </NextMatchProvider>
     </Router>
